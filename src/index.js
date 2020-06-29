1388b3c0a6fe33fdb6a46071151690a21bb4a9c1
Jorge <jorgeatgu@gmail.com>
2020-06-29 23:31:17 +0200

    refactor colors | add loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { xmlToJson } from "./helpers.js";
 
 const { SNOWPACK_PUBLIC_GOODREADS_APP_KEY } = import.meta.env;
-let userGoodReads = '67134749';
-let arrayColors = [];
+let userGoodReads = '';
+let arrayColors = ["rgb(96,28,136)", "rgb(125,162,193)", "rgb(17,105,102)", "rgb(47,221,206)", "rgb(15,94,176)", "rgb(242,176,246)", "rgb(199,98,142)", "rgb(233,55,168)", "rgb(87,80,90)", "rgb(163,105,219)"]
 let booksClean = [];
-
-const url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/${userGoodReads}.xml?key=${SNOWPACK_PUBLIC_GOODREADS_APP_KEY}&v=2&v=2&shelf=ALL&page=1&per_page=200`;
+const spinnerDiv = document.getElementById('ringlera-loader')
 
 const config = {
   method: 'GET',
@@ -15,20 +14,30 @@ const config = {
   },
 };
 
-//Get data from the API-MONOLITO-XML. We are in 2020, right?
-async function getUserAsync() {
-  let response = await fetch(url, config);
-  let data = await response.text()
-  return data;
+document.getElementById('ringlera-btn-build').addEventListener("click", getIdFromInput);
+
+function getIdFromInput() {
+  userGoodReads = document.getElementById('ringlera-input-user').value
+  spinnerDiv.style.display = 'block'
+  const url = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/${userGoodReads}.xml?key=${SNOWPACK_PUBLIC_GOODREADS_APP_KEY}&v=2&v=2&shelf=ALL&page=1&per_page=200`;
+
+  //Get data from the API-MONOLITO-XML. We are in 2020, right?
+  async function getUserAsync() {
+    let response = await fetch(url, config);
+    let data = await response.text()
+    return data;
+  }
+
+  getUserAsync()
+    .then(data => {
+      console.log("data", data);
+      const parseString = new DOMParser().parseFromString(data, 'application/xml');
+      const jsonFromXml = xmlToJson(parseString);
+      console.log("jsonFromXml", jsonFromXml);
+
+      cleanResponse(jsonFromXml);
+    });
 }
-
-getUserAsync()
-  .then(data => {
-    const parseString = new DOMParser().parseFromString(data, 'application/xml');
-    const jsonFromXml = xmlToJson(parseString);
-
-    cleanResponse(jsonFromXml);
-  });
 
 //I only want the books that the user has finished reading
 function cleanResponse(json) {
@@ -50,16 +59,20 @@ function formatBooksObject(books) {
   books.forEach((d) => {
     d.title = Object.values(d.book.title)[0];
     d.pages = Object.values(d.book.num_pages)[0];
-    d.started = FFDR(d.started_at);
-    d.read = FFDR(d.read_at);
+    d.started = parseStrangeDateRailsAPI(d.started_at);
+    d.read = parseStrangeDateRailsAPI(d.read_at);
     d.days = Math.abs(d.read - d.started) / 86400000;
     d.average = d.pages / d.days;
   });
+  spinnerDiv.style.display = 'none'
+  const mainDiv = document.querySelector('main')
+  mainDiv.style.display = 'block'
+  mainDiv.scrollIntoView({behavior: "smooth"});
   createNewObjectBooks(books);
 }
 
 //WTF: we need a function to format date XML-Rails API, kill me madafaka.
-function FFDR(string) {
+function parseStrangeDateRailsAPI(string) {
   const splitDate = (stringDate) => stringDate.split(' ');
 
   const first = Object.values(string);
@@ -80,10 +93,12 @@ function createNewObjectBooks(books) {
       const { title, pages, days, started, average } = item;
       const theDate = new Date(started);
       let myNewDate = new Date(theDate);
+      let getYearBook = myNewDate.getFullYear()
       myNewDate.setDate(myNewDate.getDate() + index);
       booksClean.push({
         title: title,
         day: myNewDate,
+        year: getYearBook,
         pages: pages,
         pagesReaded: average,
         days: days,
@@ -91,38 +106,12 @@ function createNewObjectBooks(books) {
     }
   }
 
-  getRandomColor(books.length);
+  createLineChart(booksClean);
 }
-
-function getRandomColor(elements) {
-  const promiseColors = fetch(
-    `http://www.colr.org/json/colors/random/${elements}`
-  );
-
-  promiseColors
-    .then((response) => response.json())
-    .then((datos) => {
-      addElement(datos);
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
-}
-
-function addElement(datos) {
-  const { colors: colors } = datos;
-
-  for (let item of colors) {
-    arrayColors.push(`#${item.hex}`);
-  }
-
-  createLineChart(booksClean, arrayColors);
-}
-
 
 //And finally I can create a chart, happy ends...
-function createLineChart(datos, colors) {
-  const margin = { top: 24, right: 24, bottom: 24, left: 24 };
+function createLineChart(datos) {
+  const margin = { top: 24, right: 24, bottom: 24, left: 48 };
   let width = 0;
   let height = 0;
   const chart = d3.select('.ringlera-chart');
@@ -153,26 +142,27 @@ function createLineChart(datos, colors) {
   }
 
   function updateScales(width, height) {
-    scales.count.x.range([0, width]);
-    scales.count.y.range([height, 0]);
+    const { count: { x, y } } = scales
+    x.range([0, width]);
+    y.range([height, 0]);
   }
 
   function drawAxes(g) {
-    const axisX = d3.axisBottom(scales.count.x);
+    const { count: { x, y } } = scales
+    const axisX = d3.axisBottom(x);
 
     g.select('.axis-x').attr('transform', `translate(0,${height})`).call(axisX);
 
     const axisY = d3
-      .axisLeft(scales.count.y)
-      .tickFormat(d3.format('d'))
+      .axisLeft(y)
+      .tickFormat(d => `${d} pp.`)
       .ticks(5)
       .tickSizeInner(-width);
 
     g.select('.axis-y').call(axisY);
   }
 
-  function updateChart(datos, colors) {
-    console.log("datos", datos);
+  function updateChart(datos) {
     const w = chart.node().offsetWidth;
     const h = 600;
 
@@ -196,7 +186,7 @@ function createLineChart(datos, colors) {
 
     const container = chart.select('.ringlera-chart-container-dos');
 
-    const color = d3.scaleOrdinal(colors);
+    const color = d3.scaleOrdinal(arrayColors);
 
     const line = d3
       .line()
@@ -209,33 +199,31 @@ function createLineChart(datos, colors) {
       container
         .append('path')
         .attr('class', 'book')
-        .style('stroke', () => (d.color = color(d.key)))
+        .style('stroke', () => (d.color = color(d.values[0].year)))
         .attr('d', line(d.values))
         .on('mouseover', () => {
-          const positionX = scales.count.x(d.year);
-          const postionWidthTooltip = positionX + 270;
-          const tooltipWidth = 210;
-          const positionleft = `${d3.event.pageX}px`;
-          const positionright = `${d3.event.pageX - tooltipWidth}px`;
-          tooltip.transition();
+          const positionX = scales.count.x(d.values[0].day);
+          const tooltipMargin = 50;
+          const positionTooltip = `${positionX + tooltipMargin}px`;
+          tooltip.transition().duration(300);
           tooltip.attr('class', 'tooltip tooltip-book');
           tooltip
             .style('opacity', 1)
             .html(
               `<h3 class="tooltip-book-title">${
                 d.values[0].title
-              }</h3><p class="tooltip-book-text">Te ha costado ${d.values[0].days.toFixed(
-                0
-              )} días leerte las ${d.values[0].pages} páginas.<p/>`
+              }</h3>
+              <p class="tooltip-book-text"><strong>Days:</strong> ${d.values[0].days.toFixed(0)} <p/>
+              <p class="tooltip-book-text"><strong>Pages:</strong> ${d.values[0].pages} <p/>`
             )
             .style(
               'left',
-              postionWidthTooltip > w ? positionright : positionleft
+              positionTooltip
             )
-            .style('top', `${d3.event.pageY - 28}px`);
+            .style('top', `${scales.count.y(d.values[0].pages)}px`);
         })
         .on('mouseout', () => {
-          tooltip.transition().duration(200).style('opacity', 0);
+          tooltip.transition().duration(300).style('opacity', 0);
         });
     });
 
@@ -249,7 +237,7 @@ function createLineChart(datos, colors) {
   function loadData() {
     setupElements();
     setupScales();
-    updateChart(datos, colors);
+    updateChart(datos);
   }
 
   window.addEventListener('resize', resize);
